0d0d1742fe43fe397fbfb0336665b16bf675b84c
Akhilanandateja <akhilanandateja@gmail.com>
2026-04-17 14:02:12 +0530

    added images

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ rates={
 }
 def calculate_fare(km,type,hour):
     base=rates[type]*km
+    surge_charge=0
     if hour>=17 and hour<=20:
-        base=base*1.5
-    return round(base,2)
+        surge_charge=base*0.5
+        base=base+surge_charge
+    return round(base,2),round(surge_charge,2)
 print("===== CAB FARE CALCULATOR =====")
 try:
     km=float(input("Enter distance in km: "))
@@ -30,10 +32,12 @@ try:
         print("Service not available for the selected vehicle type")
     else:
         base_fare=rates[type]*km
-        total_fare=calculate_fare(km,type,hour)
+        total_fare,surge_charge=calculate_fare(km,type,hour)
+        discount=0
         discount_applied=False
         if km>=20:
-            total_fare=total_fare*0.9
+            discount=total_fare*0.1
+            total_fare=total_fare-discount
             discount_applied=True
         min_fare=False
         if total_fare<50:
@@ -43,13 +47,13 @@ try:
         print("Distance:",km,"km")
         print("Vehicle:",type)
         print("Time:",hour)
-        print("Base Fare:",base_fare)
+        print("Base Fare: Rs.",base_fare)
         if discount_applied:
-            print("--Long distance discount is applied--")
+            print("--Long distance discount is applied: - Rs.",round(discount,2))
         if min_fare:
-            print("--Minimum fare charges applied--")
-        if hour>=17 and hour<=20:
-            print("--Peak Hour Surcharge Applied--")
-        print("Total Fare:",round(total_fare,2))
+            print("--Minimum fare charges applied")
+        if surge_charge>0:
+            print("--Peak Hour Surcharge Applied: + Rs.",round(surge_charge,2))
+        print("Total Fare: Rs.",round(total_fare,2))
 except ValueError:
     print("Invalid input, enter a numeric input")
